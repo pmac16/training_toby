0b276862811c4ba1140afaf10bf0dbe645f12016
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2021-06-16 19:38:49 -0700

    created BlogPost header

--- blog/src/components/BlogPost/index.js
+++ blog/src/components/BlogPost/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../UI/Card";
 import "./style.css";
+import Toby1 from "../../assets/blog/Toby1.JPG";
 
 /**
  * @author
@@ -8,7 +9,20 @@ import "./style.css";
  **/
 
 const BlogPost = (props) => {
-  return <Card style={{ width: "70%" }}>Meet Toby and Priya</Card>;
+  return (
+    <div className="blogPostContainer">
+      <Card>
+        <div className="blogHeader">
+          <span className="blogCategory">Featured</span>
+          <h1 className="postTitle">Beautiful is always beautiful</h1>
+          <span className="postedBy">Priya Macpherson ⋅ 06.16.21</span>
+        </div>
+        <div className="postImageContainer">
+          <img src={Toby1} />
+        </div>
+      </Card>
+    </div>
+  );
 };
 
 export default BlogPost;
